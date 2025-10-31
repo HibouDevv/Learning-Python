618c71769f7787f11ec5528367df6a1b5055e65f
HibouDevv <ahmed.rarhibou@outlook.com>
2025-10-30 20:38:37 -0400

    Added more files, will later organize them

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 # This is a Python comment in my first Python app.
 
+# This is how to create a variable in Python
+variablename = value
+
+# This won't work:
+value = variablename
+
 # This variable contains an integer
 quantity = 14
 
@@ -11,3 +17,12 @@ extended_price = quantity * unit_price
 
 # Show the results
 print(extended_price)
+
+# This is acceptable:
+first_name = "Alan"
+last_name = "Simpson"
+print(first_name, last_name)
+
+# And so is this:
+first_name = "Alan"; last_name = "Simpson"
+print(first_name, last_name)
--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,7 @@
 print("Hello World")
 
-''' This would not work:
-PRINT("Hello World") '''
+# This would not work:
+PRINT("Hello World")
 
+# How to add an apostrophe in a string
 print('Sam\'s dog said woof')
